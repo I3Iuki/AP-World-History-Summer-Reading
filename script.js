98d65d0be52594b2ec6b89a5783cb0aa094015e9
I3Iuki <nguyenkien20101011@gmail.com>
2026-08-22 13:04:48 -0400

    8/21/26 1:42am

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const sectionImages = document.querySelectorAll('section img');
+// section images
+const sectionImages = document.querySelectorAll('section-img');
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting && !entry.target.classList.contains('fade')) {
             entry.target.classList.add('fade'); 
             observer.unobserve(entry.target);
-            console.log("💩");
-        }        
+        }   
     });
 })
 
 sectionImages.forEach(img => {
     observer.observe(img);
 }); 
+
+
+// header slideshow
+const slideshowImages = document.querySelectorAll('.slideshow-img');
+
